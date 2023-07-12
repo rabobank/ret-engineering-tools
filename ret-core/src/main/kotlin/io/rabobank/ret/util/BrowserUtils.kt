7deb726e57704be77bf315c3693eb38f70962503
Martin Kanters <mkanters93@gmail.com>
2023-07-12 11:05:07 +0200

    Add the possibility to open URLs instead of Strings.

--- ret-core/src/main/kotlin/io/rabobank/ret/util/BrowserUtils.kt
+++ ret-core/src/main/kotlin/io/rabobank/ret/util/BrowserUtils.kt
@@ -4,6 +4,7 @@ import jakarta.enterprise.context.ApplicationScoped
 import org.apache.commons.lang3.SystemUtils.IS_OS_LINUX
 import org.apache.commons.lang3.SystemUtils.IS_OS_MAC
 import org.apache.commons.lang3.SystemUtils.IS_OS_WINDOWS
+import java.net.URL
 
 /**
  * Utils to open a Browser
@@ -22,6 +23,13 @@ class BrowserUtils {
             IS_OS_LINUX -> throw NotImplementedError("Linux support is not available yet.")
             IS_OS_MAC -> openUrlMacOs(url)
         }
+    }
+
+    /**
+     * Open the provided [url] in a browser.
+     */
+    fun openUrl(url: URL) {
+        openUrl(url.toString())
     }
 
     private fun openUrlWindows(url: String) {
