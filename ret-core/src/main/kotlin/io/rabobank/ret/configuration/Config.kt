7d16e620d847ff01c139c34743aa5112a5f0298c
Martin Visser <martin.visser01@rabobank.nl>
2023-08-22 13:12:10 +0200

    Split initialize and configure into two separate sub-commands

--- ret-core/src/main/kotlin/io/rabobank/ret/configuration/Config.kt
+++ ret-core/src/main/kotlin/io/rabobank/ret/configuration/Config.kt
@@ -3,20 +3,9 @@ package io.rabobank.ret.configuration
 import java.nio.file.Path
 
 interface Config {
-    operator fun get(key: String): String?
-    operator fun set(key: String, value: String)
+    operator fun get(key: String): Any?
+    operator fun set(key: String, value: Any?)
     fun configure(function: (ConfigurationProperty) -> Unit)
     fun configFile(): Path
-    fun prompt(function: (Question) -> Answer): List<Answer>
+    fun pluginConfigDirectory(): Path
 }
-
-data class Question(
-    val key: String,
-    val prompt: String,
-    val required: Boolean = false,
-)
-
-data class Answer(
-    val key: String,
-    val answer: String,
-)
--- ret-plugin/src/main/java/io/rabobank/ret/RetPlugin.java
+++ ret-plugin/src/main/java/io/rabobank/ret/RetPlugin.java
@@ -2,44 +2,52 @@ package io.rabobank.ret;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.quarkus.logging.Log;
 import io.quarkus.picocli.runtime.PicocliRunner;
 import io.quarkus.runtime.Quarkus;
 import io.rabobank.ret.jni.JClass;
 import io.rabobank.ret.jni.JNIEnvironment;
-import io.rabobank.ret.jni.JNINativeInterface;
 import io.rabobank.ret.jni.JString;
 import org.graalvm.nativeimage.IsolateThread;
 import org.graalvm.nativeimage.c.function.CEntryPoint;
-import org.graalvm.nativeimage.c.type.CCharPointer;
 import org.graalvm.nativeimage.c.type.CTypeConversion;
 
+@SuppressWarnings("unused")
 public final class RetPlugin {
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
     private RetPlugin() {
 
     }
 
     @CEntryPoint(name = "Java_io_rabobank_ret_plugins_RetPlugin_invoke")
-    public static void invoke(JNIEnvironment jniEnv, JClass clazz, @CEntryPoint.IsolateThreadContext long isolateId, JString dataCharStr) {
-        JNINativeInterface fn = jniEnv.getFunctions();
-        CCharPointer charptr = fn.getGetStringUTFChars().call(jniEnv, dataCharStr, (byte) 0);
-        final String resultString = CTypeConversion.toJavaString(charptr);
+    public static void invoke(final JNIEnvironment jniEnv,
+                              final JClass clazz,
+                              @CEntryPoint.IsolateThreadContext final long isolateId,
+                              final JString dataCharStr) {
+        final var fn = jniEnv.getFunctions();
+        final var charptr = fn.getGetStringUTFChars().call(jniEnv, dataCharStr, (byte) 0);
+        final var resultString = CTypeConversion.toJavaString(charptr);
 
         try {
-            RetContext retContext = new ObjectMapper().readValue(resultString, RetContext.class);
+            var retContext = OBJECT_MAPPER.readValue(resultString, RetContext.class);
             PluginConfiguration.RET_CONTEXT_INSTANCE = retContext;
             Quarkus.run(PicocliRunner.class, retContext.getCommand().toArray(new String[0]));
         } catch (JsonProcessingException e) {
-            e.printStackTrace();
+            Log.error(e.getMessage(), e);
         }
     }
 
     @CEntryPoint(name = "Java_io_rabobank_ret_plugins_RetPlugin_initialize")
-    public static void initialize(JNIEnvironment jniEnv, JClass clazz, @CEntryPoint.IsolateThreadContext long isolateId, JString dataCharStr) {
-        JNINativeInterface fn = jniEnv.getFunctions();
-        CCharPointer charptr = fn.getGetStringUTFChars().call(jniEnv, dataCharStr, (byte) 0);
-        final String resultString = CTypeConversion.toJavaString(charptr);
+    public static void initialize(final JNIEnvironment jniEnv,
+                                  final JClass clazz,
+                                  @CEntryPoint.IsolateThreadContext final long isolateId,
+                                  final JString dataCharStr) {
+        final var fn = jniEnv.getFunctions();
+        final var charptr = fn.getGetStringUTFChars().call(jniEnv, dataCharStr, (byte) 0);
+        final var args = CTypeConversion.toJavaString(charptr);
 
-        Quarkus.run(PicocliRunner.class, "initialize", resultString);
+        Quarkus.run(PicocliRunner.class, "initialize", args);
     }
 
     @CEntryPoint(name = "Java_io_rabobank_ret_plugins_RetPlugin_createIsolate", builtin = CEntryPoint.Builtin.CREATE_ISOLATE)
