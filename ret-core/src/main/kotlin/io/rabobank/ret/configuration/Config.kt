ef06eec7b46091060c5c454fcb8ea23f90d1777b
Martin Visser <martin.visser01@rabobank.nl>
2023-09-04 15:18:31 +0200

    Drop BasePluginConfig and convert interface Configurable into class for simplicity

--- ret-core/src/main/kotlin/io/rabobank/ret/configuration/Config.kt
+++ ret-core/src/main/kotlin/io/rabobank/ret/configuration/Config.kt
@@ -5,6 +5,7 @@ import java.nio.file.Path
 interface Config {
     operator fun get(key: String): Any?
     operator fun set(key: String, value: Any?)
+    fun load(): PluginConfig
     fun configure(function: (ConfigurationProperty) -> Unit)
     fun configFile(): Path
     fun pluginConfigDirectory(): Path
