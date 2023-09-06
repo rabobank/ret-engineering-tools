6799de91cdf550d47a38d538f0b19891570a074a
Martin Visser <martin.visser01@rabobank.nl>
2023-09-06 15:36:24 +0200

    Store ret-core version in ret.json

--- ret-cli/src/main/kotlin/io/rabobank/ret/context/ExecutionContext.kt
+++ ret-cli/src/main/kotlin/io/rabobank/ret/context/ExecutionContext.kt
@@ -1,12 +1,14 @@
 package io.rabobank.ret.context
 
+import io.rabobank.ret.configuration.version.VersionProperties
 import jakarta.enterprise.context.ApplicationScoped
 
 @ApplicationScoped
-class ExecutionContext {
+class ExecutionContext(private val versionProperties: VersionProperties = VersionProperties()) {
 
     private val gitContext = GitContext.create()
 
     fun repositoryName() = gitContext?.repositoryName()
     fun branchName() = gitContext?.branchName()
+    fun version() = versionProperties.getAppVersion()
 }
